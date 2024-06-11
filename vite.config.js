78682afd83f29d01dd2cf684b681d5157a3cb9a1
obafisayo-alx <sekaniisjoy@gmail.com>
2024-06-11 01:27:14 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['slick-carousel/slick/slick.css']
+      external: [
+        'slick-carousel/slick/slick.css',
+        'slick-carousel/slick/slick-theme.css'
+      ]
     }
   }
 })
