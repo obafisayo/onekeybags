97b7dd9336db6fd906df8cc731b431e24076518a
obafisayo-alx <sekaniisjoy@gmail.com>
2024-06-11 01:00:37 +0100

    comit again jare

--- src/pages/LandingPage/home/Home.jsx
+++ src/pages/LandingPage/home/Home.jsx
@@ -1,9 +1,9 @@
-import { Container } from "postcss";
+import Container from "../../../components/container/Container";
 import HeroSection from "../../../components/heroSection/HeroSection";
 
 function Home() {
     return (
-        <Container>
+        <Container className="container">
             <HeroSection />
         </Container>
     );
