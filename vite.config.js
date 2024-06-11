aecc2a213673bcfedd0a54acba974916095c37cd
obafisayo-alx <sekaniisjoy@gmail.com>
2024-06-11 01:35:17 +0100

    vite errors

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
+        'react-slick',
         'slick-carousel/slick/slick.css',
         'slick-carousel/slick/slick-theme.css'
       ]
